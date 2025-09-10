f9cbabffd98a2190769140376f1f7888b63e666c
Fesomibe <francisesomibe1@gmail.com>
2025-09-10 03:00:54 -0500

    show trending movies

--- services/appwrite.ts
+++ services/appwrite.ts
@@ -47,3 +47,18 @@ export const updateSearchCount = async (query: string, movie: Movie) => {
     // if no document is found, 
         // create a new document in Appwrite database -> 1
 };
+
+export const getTrendingMovies = async (): Promise<TrendingMovie[] | undefined> => {
+    try {
+        const result = await databases.listDocuments(DATABASE_ID, COLLECTION_ID, [
+        Query.limit(5),
+        Query.orderDesc('count'),
+    ]);
+
+    return result.documents as unknown as TrendingMovie[];
+    } catch (error) {
+        console.log(error);
+        return undefined;
+    }
+    
+}
